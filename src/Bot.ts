5e4d35ceeac637a9a032c9c28604a2c810ad5b0e
Jewel Lambert <jewel@originalspin.dev>
2023-08-31 09:47:11 -0400

    fixed src path issues

--- src/Bot.ts
+++ src/Bot.ts
@@ -8,11 +8,13 @@ console.log("Bot is starting...");
 const client = new Client({ intents: [GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages] });
 
 
+const srcDir = path.resolve(__dirname, '../src/');
+
 // Dynamically set up listeners from the listeners directory
-const listenerFiles = fs.readdirSync(path.join(__dirname, 'listeners')).filter(file => file.endsWith('.ts'));
+const listenerFiles = fs.readdirSync(path.join(srcDir,'listeners')).filter(file => file.endsWith('.ts'));
 
 for (const file of listenerFiles) {
-    const listener = require(path.join(__dirname, 'listeners', file)).default;
+    const listener = require(path.join(srcDir, 'listeners', file)).default;
     listener(client);
 }
 
--- src/Commands.ts
+++ src/Commands.ts
@@ -1,15 +1,16 @@
 import { Command } from "./Command";
 import fs from "fs";
 import path from "path";
+const srcDir = path.resolve(__dirname, '../src/');
 
 // Get all command files from the commands directory
-const commandFiles = fs.readdirSync(path.join(__dirname, 'commands')).filter(file => file.endsWith('.ts'));
+const commandFiles = fs.readdirSync(path.join(srcDir, 'commands')).filter(file => file.endsWith('.ts'));
 
 const Commands: Command[] = [];
 
 // Dynamically import each command and add it to the Commands array
 for (const file of commandFiles) {
-    const command: Command = require(path.join(__dirname, 'commands', file)).default;
+    const command: Command = require(path.join(srcDir, 'commands', file)).default;
     Commands.push(command);
 }
 
