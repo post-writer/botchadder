1d9c97a2c7914de4540b4c2ba6fad0b67d2c6b0a
Jewel Lambert <jewel@originalspin.dev>
2023-08-31 10:12:13 -0400

    must search for .ts files, but remove the extension

--- src/Bot.ts
+++ src/Bot.ts
@@ -5,17 +5,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 console.log("Bot is starting...");
-const client = new Client({ intents: [GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages] });
-
-
-const srcDir = path.resolve(__dirname, '../src/');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
 
 // Dynamically set up listeners from the listeners directory
-const listenerFiles = fs.readdirSync(path.join(srcDir,'listeners')).filter(file => file.endsWith('.ts'));
+const listenerFiles = fs.readdirSync(path.join(__dirname,'../src/listeners')).filter(file => file.endsWith('.ts')).map(file => file.replace('.ts', ''));
 
 for (const file of listenerFiles) {
-    const listener = require(path.join(srcDir, 'listeners', file)).default;
+    const listener = require(path.join(__dirname, 'listeners', file)).default;
     listener(client);
 }
 
 client.login(process.env.TOTALLYHUMAN_DISCORD_BOT_TOKEN);
+
--- src/Commands.ts
+++ src/Commands.ts
@@ -1,16 +1,15 @@
 import { Command } from "./Command";
 import fs from "fs";
 import path from "path";
-const srcDir = path.resolve(__dirname, '../src/');
 
 // Get all command files from the commands directory
-const commandFiles = fs.readdirSync(path.join(srcDir, 'commands')).filter(file => file.endsWith('.ts'));
+const commandFiles = fs.readdirSync(path.join(__dirname, '../src/commands')).filter(file => file.endsWith('.ts')).map(file => file.replace('.ts', ''));
 
 const Commands: Command[] = [];
 
 // Dynamically import each command and add it to the Commands array
 for (const file of commandFiles) {
-    const command: Command = require(path.join(srcDir, 'commands', file)).default;
+    const command: Command = require(path.join(__dirname, 'commands', file)).default;
     Commands.push(command);
 }
 
